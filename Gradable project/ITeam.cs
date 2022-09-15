304808140202b991b3c4fc0344c5a0c20e385d2c
pravallikaadineni <pravallikaadineni143@gmail.com>
2022-09-15 17:46:56 +0530

    upload

---
+++ Gradable project/ITeam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using player_and_team;
+
+namespace player_and_team
+{
+    public interface ITeam
+    {
+        void Add(player player);
+        void Remove(int playerId);
+         void GetPlayerById(int playerId);
+        void GetPlayerByName(string playerName);
+        void GetAllPlayers( );
+
+
+
+
+
+
+
+    }
+}
---
+++ Gradable project/player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player_and_team
+{
+    public class player
+    {
+        private int _PlayeriD;
+        public int PlayerID
+        {
+            get { return _PlayeriD; }
+            set { _PlayeriD = value; }
+        }
+
+        private string _playername;
+
+        public string PlayerName
+        {
+            get { return _playername; }
+            set { _playername = value; }
+        }
+        private int _playerage;
+
+        public int PlayerAge
+        {
+            get { return _playerage; }
+            set { _playerage = value; }
+        }
+    }
+}
+
